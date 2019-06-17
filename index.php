4eca1ada32afafac3816e5fac8a7a74730e4ce9c
sor7535 <sor7535@gmail.com>
2019-06-17 22:40:22 +0100

    switch

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Soramist Chintanamanus</title>
+	
+	<!-- Sorchill CSS -->
+	<link rel="stylesheet" href="_css/sorchill5.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+	<script>WebFont.load({google: {families: ["Montserrat:400,700","Roboto:300,regular,500"]}});</script>
+	
+	<link rel="shortcut icon" href="_images/favicon.ico" />
+	
+	
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	<!-- Hovering on mobile -->
+	<script src="_js/mobile_touch.js"></script>
+	
+</head>
+	
+	
+<body>
+
+<?php include_once("_php/analyticstracking.php") ?>
+
+	<div class="home-header">
+		<div class="container">
+			<a href="#"><h1 class="site-name">SORAMIST C.</h1></a>
+		</div>
+	</div>
+	
+	
+	<div class="section-about">
+		<div class="container">
+			<div class="content">
+				<div class="about-image" data-toggle="collapse" data-target="#about-collapse">
+				<!--<div class="title overtext"> about </div>-->
+				<!--<img style="cursor: pointer;" class="about-image" src="_images/about_s.png" alt="About" data-toggle="collapse" data-target="#about-collapse"/>-->
+				</div>
+				
+				
+				<!--Collapsible Panal-->
+				<div id="about-collapse" class="panel-collapse collapse">
+					<div class="about-hidden">
+						<div class="about-title">Hello!</div>
+						<p>My name is Soramist. I'm originally from Bangkok, Thailand. I had spent some years studying and working in Sweden, Australia, California, and currently I'm based in London working for Skyscanner on some amazing projects.</p> 
+						
+						<p>I’m a designer, an engineer, an entrepreneur, a critical thinker, a problem solver (and sometimes a troublemaker). I’m a better listener than talker. I like to understand rather than to judge. I love simple things that work delightfully. I have a strong passion to create meaningful product and service that improve people's lives. And I believe good design can create a better world.</p>
+						
+						<p>Below are some of the projects I’ve done along my journey. <a href="mailto:soramist@gmail.com">Drop me a line</a> if you would like to hear more about them.
+						</p>
+						
+						<h1><a href="_images/CV_Soramist.pdf" target="window">DOWNLOAD CV</a></h1>
+						
+						<div class="about-social">
+							<a href="https://vimeo.com/sorchill" target="window">
+							<img src="_images/social_vimeo.png" alt="sorchill.motion" />
+							</a>
+							
+							<a href="https://uk.pinterest.com/sorchill/" target="window">
+							<img src="_images/social_pinterest.png" alt="Pinterest" />
+							</a>
+							
+							<a href="https://uk.linkedin.com/in/soramist-chintanamanus-45299413" target="window">
+							<img src="_images/social_linkedin.png" alt="LinkedIn" />
+							</a>
+							
+							<a href="mailto:soramist@gmail.com" target="window">
+							<img src="_images/social_mail.png" alt="MailMe" />
+							</a>
+						</div>
+						
+						<div class="line-separator"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	<div class="section-gallery">
+		<div class="container">
+			<div class="content">
+				
+				<div class="gallery-box">
+					<a href="projects/skyscanner_explorer_experience.php">
+					<img src="_images/gallery_see_2.jpg" alt="Skyscanner Explore" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Skyscanner Explore </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/skype_ios.php">
+					<img src="_images/gallery_skype_ios.png" alt="Skype for iOS" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Skype for iOS </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/skype_qik.php">
+					<img src="_images/gallery_skype_qik.png" alt="Skype Qik" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Skype Qik </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/skype_tv.php">
+					<img src="_images/gallery_skype_tv.jpg" alt="Skype on TV" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Skype on TV </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/lilla.php">
+					<img src="_images/gallery_lilla.jpg" alt="Den Lilla Nod Samlaren" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Piggy Bank </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/localalike.php">
+					<img src="_images/gallery_localalike.png" alt="Local Alike" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Local Alike </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<!--<div class="gallery-box">
+					<a href="projects/sweatify.php">
+					<img src="_images/gallery_sweatify_1.png" alt="Sweatify" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Sweatify </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>-->
+				
+				<div class="gallery-box">
+					<a href="projects/sync.php">
+					<img src="_images/gallery_sync.png" alt="The Sync" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> The Sync </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/experience.php">
+					<img src="_images/gallery_exp.jpg" alt="Experience Prototype" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> EXP Prototype </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/amazon.php">
+					<img src="_images/gallery_amazon.png" alt="AmazonPee();" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> AmazonPee(); </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/paper.php">
+					<img src="_images/gallery_paper.jpg" alt="Data Visualisation" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Data Viz </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/uid.php">
+					<img src="_images/gallery_uid.jpg" alt="UID Redemption" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Storytelling </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/service.php">
+					<img src="_images/gallery_service.jpg" alt="Service Design" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Service Design </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/bench.php">
+					<img src="_images/gallery_bench.jpg" alt="Sound+Light+Form" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Sound/Light/Form </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				<div class="gallery-box">
+					<a href="projects/multi.php">
+					<img src="_images/gallery_multi.jpg" alt="Multi-Layering" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Multi-Layering </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>
+				
+				
+				<!--<div class="gallery-box">
+					<a href="projects/kitchen.php">
+					<img src="_images/gallery_kitchen.jpg" alt="Kitchen Mate" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Kitchen Mate </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>-->
+				
+				<!--<div class="gallery-box">
+					<a href="projects/sweatify.php">
+					<img src="_images/gallery_sweatify.png" alt="Sweatify" />
+					<div class="gallery-overlay">
+					    <div class="title overtext"> Sweatify </div>
+					    <div class="tagline overtext"> </div>
+					</div>
+					</a>
+				</div>-->
+
+				
+			</div>
+		</div>
+	</div>
+	<div class="separator-mini"></div>
+	<div class="line-separator"></div>
+	
+	<!-- Footer -->
+	<?php include '_php/footer.php'; ?>
+	
+	
+	
+</body>
+	
+</html>
+		
+
+
+
+
+
+
+
